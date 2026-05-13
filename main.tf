31105093232b9dfe14303f4b494b0a4f849c7d27
mansoor <sayyadmansoor@gmail.com>
2026-05-13 06:29:21 +0000

    Added private subnet

--- main.tf
+++ main.tf
@@ -18,6 +18,18 @@ resource "aws_subnet" "public_subnet" {
     Name = "public-subnet"
   }
 }
+resource "aws_subnet" "private_subnet" {
+
+  vpc_id = aws_vpc.main_vpc.id
+
+  cidr_block = "10.0.2.0/24"
+
+  availability_zone = "ap-south-1a"
+
+  tags = {
+    Name = "private-subnet"
+  }
+}
 resource "aws_internet_gateway" "igw" {
 
   vpc_id = aws_vpc.main_vpc.id
